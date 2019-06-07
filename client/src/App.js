eeecc39b85dfb4ba2f61ba96b2851574fbeddf63
gem007bd <gem007bd@yahoo.com>
2019-06-07 15:22:55 +0200

    add server and client

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import { Launches } from './components/Launches';
 import './App.css';
+import './theme/bootstrap-cyborg.min.css';
+import logo from './spacex-logo.jpg';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql'
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
+    <div className = "App">    
+        <div className="container">
+          <ApolloProvider client = {client}> 
+            <img src={logo} alt="spacex logo" style={{width: 300, display: 'block', margin: 'auto'}}></img>
+            <Launches />
+          </ ApolloProvider>
+        </div>
     </div>
   );
 }
 
+
 export default App;
+
