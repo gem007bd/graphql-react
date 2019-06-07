3bfa7ecf81a6607cd251f5244852b59376622b10
gem007bd <gem007bd@yahoo.com>
2019-06-07 16:03:42 +0200

    add launchitem component and ui fix

--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -1,6 +1,7 @@
-import React, { Component }  from 'react';
+import React, { Component, Fragment }  from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
+import LaunchItem from './LaunchItem';
 
 
 const LAUNCHES_QUERY = gql`
@@ -17,7 +18,7 @@ const LAUNCHES_QUERY = gql`
 export class Launches extends Component {
     render() {
         return (
-            <div>
+            <Fragment>
                 <h1 className="display-4 space-left2">Launches</h1>
                 <Query query={LAUNCHES_QUERY}>
                     {
@@ -25,11 +26,21 @@ export class Launches extends Component {
                             if(loading) return <h4>Loading ...</h4>
                             if(error) console.log(error);
                             console.log(data);
-                            return <h1>test</h1>
+                            return (
+                                < Fragment > {
+                                        data.launches.map(launch => (
+                                            < LaunchItem key = {
+                                                launch.flight_number
+                                            }
+                                            launch={launch} / >
+                                        ))
+                                    } 
+                                </Fragment>
+                            );
                         }
                     }
                 </Query>
-            </div>
+            </Fragment>
         )
     }
 }
