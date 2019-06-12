2415b08cdad73191d5ec0c1a6ae0f666ed331242
gem007bd <gem007bd@yahoo.com>
2019-06-12 14:59:27 +0200

    finish the site

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import { Launches } from './components/Launches';
+import { Launch } from './components/Launch';
 import './App.css';
 import './theme/bootstrap-cyborg.min.css';
 import logo from './spacex-logo.jpg';
@@ -12,14 +14,16 @@ const client = new ApolloClient({
 
 function App() {
   return (
-        
     <div className = "App">    
+        <BrowserRouter>
         <div className="container">
           <ApolloProvider client = {client}> 
             <img src={logo} alt="spacex logo" style={{width: 300, display: 'block', margin: 'auto'}}></img>
-            <Launches />
-          </ ApolloProvider>
+            <Route exact path='/' component={Launches} />
+            <Route exact path = '/launch/:flight_number' component = {Launch} />
+          </ ApolloProvider> 
         </div>
+        </BrowserRouter>
     </div>
   );
 }
