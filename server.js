2daeae98452ce300aff928ec038379220591369f
gem007bd <gem007bd@yahoo.com>
2019-06-12 15:47:53 +0200

    deploy ready

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use('/graphql', graphqlHTTP({
 // change for deploy
 app.use(express.static('public'));
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'public', index.html));
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000
